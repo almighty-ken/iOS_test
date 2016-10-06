577dfb06b43211cbc86ff378b2d9268143ea9c2e
Ken Cheng <kenny851017@gmail.com>
2016-10-06 16:57:04 -0400

    Initial Commit

---
+++ App-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App-Test
+//
+//  Created by Ken Cheng on 10/6/16.
+//  Copyright © 2016 KenCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
